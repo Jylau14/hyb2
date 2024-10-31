f488c474cae0e6cbb4a3802a95a54e4fd4ebef1d
Jylau14 <110675091+Jylau14@users.noreply.github.com>
2024-10-31 03:32:12 +0000

    Update cdm_indiv_zoom.R

--- bin/cdm_indiv_zoom.R
+++ bin/cdm_indiv_zoom.R
@@ -24,10 +24,10 @@ data$count2 <- ifelse(data$count > quant, quant, data$count)
 Title <- ifelse(!is.na(TITLE), TITLE, data$ID[1])
 
 cdm <- ggplot(data, aes(x,y))+
-  geom_tile(aes(fill=count2), show.legend=TRUE)+
+  geom_tile(aes(fill=count2), show.legend=TRUE, na.rm=TRUE)+
   scale_fill_gradient(na.value ="#ffffff", low = "#ffffff",high="#000000", limits=c(0,ceiling(quant)), breaks=c(as.integer(round(quant*0.25)),as.integer(round(quant/2)),as.integer(round(quant*0.75)),as.numeric(ceiling(quant))))+
   theme_bw()+theme(aspect.ratio = 1)+
-  labs(title= "Contact Density Map", subtitle = Title,x=paste0(GENE_1, " (nt)"),y=paste0(GENE_2," (nt)"))+
+  labs(title= "Contact Density Map", subtitle = Title, x= "Arm 1 (nt)", y= "Arm 2 (nt)")+
   scale_x_continuous(expand = c(0, 0), limits = c(x1, x2), breaks = c(c(x1+20), c(x2-20)), labels=c(x1, x2))+
   scale_y_continuous(expand = c(0, 0), limits = c(y1, y2), breaks = c(c(y1+20), c(y2-20)), labels=c(y1, y2))+
   theme(plot.title=element_text(size=10),plot.subtitle=element_text(size=10),text=element_text(size=9.5), axis.text=element_text(size=9))+
